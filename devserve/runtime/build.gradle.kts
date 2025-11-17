a35046d709b396896c5171eb8becb06a05b8c572
Rusty Phillips <walter.phillips@ext.airbnb.com>
2025-11-17 13:43:50 -0500

    Fixes settings.

--- devserve/runtime/build.gradle.kts
+++ devserve/runtime/build.gradle.kts
@@ -113,7 +113,7 @@ val generateGraphiQL by tasks.registering {
  *  This source code is licensed under the Apache 2.0 license found in the
  *  LICENSE file in the root directory of this source tree.
 -->
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
